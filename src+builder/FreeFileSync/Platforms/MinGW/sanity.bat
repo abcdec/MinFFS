5ced92b3c75a444703c7ce620911860fd3955e09
abcdec <abcdec@GitHub>
2014-12-28 19:21:46 -0600

    Siltent REM

--- src+builder/FreeFileSync/Platforms/MinGW/sanity.bat
+++ src+builder/FreeFileSync/Platforms/MinGW/sanity.bat
@@ -1,23 +1,22 @@
-REM // **************************************************************************
-REM // * Copyright (C) 2014 abcdec @GitHub - All Rights Reserved                *
-REM // * This file is part of a modified version of FreeFileSync, MinFFS.       *
-REM // * The original FreeFileSync program and source code are distributed by   *
-REM // * the FreeFileSync project: http://www.freefilesync.org/                 *
-REM // * This particular file is created by by abcdec @GitHub as part of the    *
-REM // * MinFFS project: https://github.com/abcdec/MinFFS                       *
-REM // *                          --EXPERIMENTAL--                              *
-REM // * This program is experimental and not recommended for general use.      *
-REM // * Please consider using the original FreeFileSync program unless there   *
-REM // * are specific needs to use this experimental MinFFS version.            *
-REM // *                          --EXPERIMENTAL--                              *
-REM // * This file is distributed under GNU General Public License:             *
-REM // * http://www.gnu.org/licenses/gpl-3.0 per the FreeFileSync License.      *
-REM // * This modified program is distributed in the hope that it will be       *
-REM // * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of *
-REM // * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU       *
-REM // * General Public License for more details.                               *
-REM // **************************************************************************
-
+@REM // **************************************************************************
+@REM // * Copyright (C) 2014 abcdec @GitHub - All Rights Reserved                *
+@REM // * This file is part of a modified version of FreeFileSync, MinFFS.       *
+@REM // * The original FreeFileSync program and source code are distributed by   *
+@REM // * the FreeFileSync project: http://www.freefilesync.org/                 *
+@REM // * This particular file is created by by abcdec @GitHub as part of the    *
+@REM // * MinFFS project: https://github.com/abcdec/MinFFS                       *
+@REM // *                          --EXPERIMENTAL--                              *
+@REM // * This program is experimental and not recommended for general use.      *
+@REM // * Please consider using the original FreeFileSync program unless there   *
+@REM // * are specific needs to use this experimental MinFFS version.            *
+@REM // *                          --EXPERIMENTAL--                              *
+@REM // * This file is distributed under GNU General Public License:             *
+@REM // * http://www.gnu.org/licenses/gpl-3.0 per the FreeFileSync License.      *
+@REM // * This modified program is distributed in the hope that it will be       *
+@REM // * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of *
+@REM // * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU       *
+@REM // * General Public License for more details.                               *
+@REM // **************************************************************************
 
 @echo off
 
