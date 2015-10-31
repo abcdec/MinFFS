a08ee5fd4ce789187d95c3c65a45f8c9032ce6c9
Your Name <you@example.com>
2015-10-31 11:43:34 -0500

    Added Windows Deep File Traverser using windows native API calls.

--- src+builder/FreeFileSync/Platforms/MinGW/MinFFS_deep_file_traverser.cpp
+++ src+builder/FreeFileSync/Platforms/MinGW/MinFFS_deep_file_traverser.cpp
@@ -78,14 +78,6 @@ namespace
 		    beforeLast(baseDirectory, FILE_NAME_SEPARATOR) :
 		    baseDirectory;
 
-		/* quote: "Since POSIX.1 does not specify the size of the d_name field, and other nonstandard fields may precede
-                   that field within the dirent structure, portable applications that use readdir_r() should allocate
-                   the buffer whose address is passed in entry as follows:
-		   len = offsetof(struct dirent, d_name) + pathconf(dirPath, _PC_NAME_MAX) + 1
-		   entryp = malloc(len); */
-#ifdef TODO_MinFFS_MinGW_no_pathconf_PC_NAME_MAX
-		const size_t nameMax = std::max<long>(::pathconf(directoryFormatted.c_str(), _PC_NAME_MAX), 10000); //::pathconf may return long(-1)
-#else
 #ifdef MinFFS_PATCH
 		// MinFFS: MinGW does not have pathconf and _PC_NAME_MAX. So
 		// use a fixed number. Since the code above will likely result
@@ -94,8 +86,14 @@ namespace
 		// anyway.  MSDN says 260 charctors.
 		// https://msdn.microsoft.com/en-us/library/windows/desktop/aa365247(v=vs.85).aspx
 		const size_t nameMax = 10000;
+#else
+		/* quote: "Since POSIX.1 does not specify the size of the d_name field, and other nonstandard fields may precede
+                   that field within the dirent structure, portable applications that use readdir_r() should allocate
+                   the buffer whose address is passed in entry as follows:
+		   len = offsetof(struct dirent, d_name) + pathconf(dirPath, _PC_NAME_MAX) + 1
+		   entryp = malloc(len); */
+		const size_t nameMax = std::max<long>(::pathconf(directoryFormatted.c_str(), _PC_NAME_MAX), 10000); //::pathconf may return long(-1)
 #endif//MinFFS_PATCH
-#endif//TODO_MinFFS_MinGW_no_pathconf_PC_NAME_MAX
 		buffer.resize(offsetof(struct ::dirent, d_name) + nameMax + 1);
 
 		traverse(directoryFormatted, sink);
@@ -114,10 +112,93 @@ namespace
 
 	void traverseWithException(const Zstring& dirPath, TraverseCallback& sink) //throw FileError
 	    {
+#ifdef MinFFS_PATCH
+		HANDLE dirHandle, fileHandle;
+		WIN32_FIND_DATA fileAttr;
+		BY_HANDLE_FILE_INFORMATION handleFileInfo;
+		Zstring findPattern = dirPath + L"\\*.*";
+		std::vector<WIN32_FIND_DATA>fileVector;
+		
+		dirHandle = ::FindFirstFile(findPattern.c_str(), &fileAttr);
+		
+		if (dirHandle == INVALID_HANDLE_VALUE) {
+		    if (::GetLastError() != ERROR_NO_MORE_FILES)
+		    {
+			throwFileError(replaceCpy(_("Cannot enumerate directory %x."), L"%x", fmtFileName(dirPath)), L"readdir_r", ::GetLastError());
+			//don't retry but restart dir traversal on error! http://blogs.msdn.com/b/oldnewthing/archive/2014/06/12/10533529.aspx
+		    }
+		    return;
+		}
+		do {
+		    fileVector.push_back(fileAttr);
+		} while (::FindNextFile(dirHandle, &fileAttr));
+		::FindClose(dirHandle);
+		
+		for (auto fileAttr_ : fileVector) {
+		    
+		    //don't return "." and ".."
+		    const Zstring shortName(fileAttr_.cFileName); //evaluate dirEntry *before* going into recursion => we use a single "buffer"!
+		    
+		    if (shortName.c_str()[0] == 0)
+		    {
+			throw FileError(replaceCpy(_("Cannot enumerate directory %x."), L"%x", fmtFileName(dirPath)), L"Data corruption: Found item without name.");
+		    }
+		    
+		    if (shortName.c_str()[0] == L'.' && (shortName.c_str()[1] == 0 || (shortName.c_str()[1] == L'.' && shortName.c_str()[2] == 0)))
+		    {
+			continue;
+		    }
+		    
+		    const Zstring& itempath = appendSeparator(dirPath) + shortName;
+		    
+		    if (fileAttr_.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) //a directory
+		    {
+			if (TraverseCallback* trav = sink.onDir({ shortName.c_str(), itempath }))
+			{
+			    ZEN_ON_SCOPE_EXIT(sink.releaseDirTraverser(trav));
+			    traverse(itempath, *trav);
+			}
+		    }
+		    else //a file or named pipe, ect.
+		    {
+			// Long file name support upto 32,767 wide characters for Unicode based path needs "\\?\" prefixed on CreateFile
+			fileHandle = CreateFile((L"\\\\?\\"+itempath).c_str(), 0, 0, NULL, OPEN_EXISTING, 0, NULL);
+			if (fileHandle == INVALID_HANDLE_VALUE)
+			{
+			    throwFileError(replaceCpy(_("Cannot read file attributes of %x."), L"%x", fmtFileName(itempath)), L"CreateFile", ::GetLastError());
+			}
+			GetFileInformationByHandle(fileHandle, &handleFileInfo);
+			CloseHandle(fileHandle);
+			
+			std::uint64_t fileSize = (static_cast<std::uint64_t>(fileAttr_.nFileSizeHigh) << 32) + static_cast<std::uint64_t>(fileAttr_.nFileSizeLow);
+			std::int64_t lastWriteWinFileTime = (static_cast<std::int64_t>(fileAttr_.ftLastWriteTime.dwHighDateTime) << 32) + static_cast<std::int64_t>(fileAttr_.ftLastWriteTime.dwLowDateTime);
+			// FILETIME structure contains a 64-bit value representing the number of 100-nanosecond intervals since January 1, 1601 (UTC).
+			// https://msdn.microsoft.com/en-us/library/windows/desktop/ms724284(v=vs.85).aspx
+			// convert to Linux epoch which is number of seconds since Jan. 1st 1970 UTC
+			std::int64_t lastWriteTimeLinuxEpoc = (lastWriteWinFileTime / 10000000LL) - 11644473600LL;
+			
+			TraverseCallback::FileInfo fi = {
+			    shortName.c_str(),              // const Zchar*
+			    itempath,                       // const Zstring&
+			    makeUnsigned(fileSize),         // std::uint64_t  unit: bytes
+			    lastWriteTimeLinuxEpoc,         // std::int64_t   number of seconds since Jan. 1st 1970 UTC
+			    extractFileId(handleFileInfo),  // const FileId&  optional: initial if not supported!
+			    nullptr                         // SymlinkInfo*   only filled if file is a followed symlink
+			};
+			sink.onFile(fi);
+		    }
+		    /*
+		      It may be a good idea to not check "S_ISREG(statData.st_mode)" explicitly and to not issue an error message on other types to support these scenarios:
+		      - RTS setup watch (essentially wants to read directories only)
+		      - removeDirectory (wants to delete everything; pipes can be deleted just like files via "unlink")
+
+		      However an "open" on a pipe will block (https://sourceforge.net/p/freefilesync/bugs/221/), so the copy routines need to be smarter!!
+		    */
+		}
+		return;
+#else
 		//no need to check for endless recursion: Linux has a fixed limit on the number of symbolic links in a path
 
-#ifdef MinFFS_PATCH
-#else
 		DIR* dirObj = ::opendir(dirPath.c_str()); //directory must NOT end with path separator, except "/"
 		if (!dirObj)
 		    throwFileError(replaceCpy(_("Cannot open directory %x."), L"%x", fmtFileName(dirPath)), L"opendir", getLastError());
@@ -214,7 +295,7 @@ namespace
 		}
 #endif
 	    }
-
+	
 	std::vector<char> buffer;
     };
 }
